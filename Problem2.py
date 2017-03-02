318343b7c3fe826a3421740cf5904567b73e040f
SeoJinhye <jyoon@sjnma.org>
2017-03-01 21:13:55 -0600

    [Problem2 complete]

--- Problem2.py
+++ Problem2.py
@@ -1,12 +1,14 @@
 import random
-secret = raw_input("write the number")
+secret = int(input("wirte the number"))
 guess = random.randint(1,1000)
 while(secret != guess):
-	if(guess > secret):
-		print("It is smaller")
+	print(guess)
+	answer = raw_input("")
+	if(answer == "big"):
+		#print(guess)
 		guess = random.randint(1,guess)
-	if(secret > guess):
-		print("It is bigger")
+	if(answer == "small"):
+		#print(guess)
 		guess = random.randint(guess,1000)
 print("You are correct! You won!")
 	
